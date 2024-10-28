6a87844623b79c4a743046e9bd8860448fc261ce
Erik Pellizzon <erikpellizzon1@gmail.com>
2024-10-29 00:38:12 +0100

    Refactor DHCPv6 client implementation

--- client/dhcp6.go
+++ client/dhcp6.go
@@ -3,6 +3,7 @@ package client
 import (
 	"context"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"github.com/insomniacslk/dhcp/dhcpv6"
 	"github.com/insomniacslk/dhcp/dhcpv6/nclient6"
@@ -11,65 +12,78 @@ import (
 	"time"
 )
 
-// DHCP6Cfg hold configuration for DHCP6Clnt
+// DHCP6Cfg holds configuration for DHCP6Clnterface
 type DHCP6Cfg struct {
 	Mac            net.HardwareAddr
 	NeedPD, NeedNA bool
 	Debug          bool
 }
 
-// DHCP6Clnt is a DHCPv6 client
-type DHCP6Clnt struct {
-	clnt                              *nclient6.Client
-	cfg                               *DHCP6Cfg
-	locaddr                           net.IP
-	ipHeader, pseudoHeader, udpHeader []byte
-	assignedIANAs                     []net.IP
-	assignedIAPDs                     []*net.IPNet
+// DHCP6Clnterface is a DHCPv6 client
+type DHCP6Clnterface struct {
+	client        *nclient6.Client
+	cfg           DHCP6Cfg
+	assignedIANAs []net.IP
+	assignedIAPDs []*net.IPNet
 }
 
-// NewDHCP6Clnt creates a new DHCPv6 client,
-// using conn as transport,
-// cfg holds the the configuration,
-// localLLA is used for local link local address for DHCPv6 msg
-func NewDHCP6Clnt(conn net.PacketConn, cfg *DHCP6Cfg, localLLA net.IP) (*DHCP6Clnt, error) {
+// NewDHCP6Clnt creates a new DHCPv6 client, using conn as transport.
+// Cfg holds the configuration, localLLA is used for local link local address for DHCPv6 msg.
+func NewDHCP6Clnt(conn net.PacketConn, cfg DHCP6Cfg) (*DHCP6Clnterface, error) {
 	var mods []nclient6.ClientOpt
 	if cfg.Debug {
 		mods = append(mods, nclient6.WithDebugLogger(), nclient6.WithLogDroppedPackets())
 	}
-	r := new(DHCP6Clnt)
-	var err error
-	r.clnt, err = nclient6.NewWithConn(conn, cfg.Mac, mods...)
+
+	client, err := nclient6.NewWithConn(conn, cfg.Mac, mods...)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create DHCPv6 client, %v", err)
+		return nil, fmt.Errorf("failed to create DHCPv6 client, %w", err)
 	}
-	r.cfg = cfg
 
-	//v6
-	r.locaddr = localLLA
-	r.ipHeader = make([]byte, 40)
-	r.ipHeader[0] = 0x60                         //version
-	r.ipHeader[6] = 17                           //next header
-	r.ipHeader[7] = 32                           //TTL
-	copy(r.ipHeader[8:24], localLLA.To16()[:16]) //src addr
-	r.pseudoHeader = make([]byte, 40)
-	copy(r.pseudoHeader[:16], localLLA.To16()[:16]) //src addr
-	r.pseudoHeader[39] = 17                         //next header
-	r.udpHeader = make([]byte, 8)
-	binary.BigEndian.PutUint16(r.udpHeader[:2], uint16(dhcpv6.DefaultClientPort)) //src port
-	r.assignedIANAs = []net.IP{}
-	r.assignedIAPDs = []*net.IPNet{}
-	return r, nil
+	return &DHCP6Clnterface{
+		cfg:    cfg,
+		client: client,
+	}, nil
 }
 
-func getIAIDviaTime(delta int64) (r [4]byte) {
-	buf := make([]byte, binary.MaxVarintLen64)
-	binary.PutVarint(buf, time.Now().UnixNano()+delta)
-	copy(r[:], buf[:4])
-	return
+// Dial completes a DHCPv6 exchange with server
+func (dc *DHCP6Clnterface) Dial(ctx context.Context) error {
+	solicitMsg, err := dc.buildSolicit()
+	if err != nil {
+		return fmt.Errorf("failed to create solicit msg for %v, %v", dc.cfg.Mac, err)
+	}
+	adv, err := dc.client.SendAndRead(
+		ctx,
+		nclient6.AllDHCPRelayAgentsAndServers, solicitMsg,
+		nclient6.IsMessageType(dhcpv6.MessageTypeAdvertise),
+	)
+	if err != nil {
+		return fmt.Errorf("failed recv DHCPv6 advertisement for %v, %v", dc.cfg.Mac, err)
+	}
+	if err := dc.checkResponse(adv, false); err != nil {
+		return fmt.Errorf("got invalid advertise msg for client %v, %v", dc.cfg.Mac, err)
+	}
+
+	request, err := newRequestFromAdv(adv)
+	if err != nil {
+		return fmt.Errorf("failed to build request msg for client %v, %v", dc.cfg.Mac, err)
+	}
+	reply, err := dc.client.SendAndRead(
+		ctx,
+		nclient6.AllDHCPRelayAgentsAndServers,
+		request, nclient6.IsMessageType(dhcpv6.MessageTypeReply),
+	)
+	if err != nil {
+		return fmt.Errorf("failed to recv DHCPv6 reply for %v, %v", dc.cfg.Mac, err)
+	}
+
+	if err := dc.checkResponse(reply, true); err != nil {
+		return fmt.Errorf("got invalid reply msg for %v, %v", dc.cfg.Mac, err)
+	}
+	return nil
 }
 
-func (dc *DHCP6Clnt) buildSolicit() (*dhcpv6.Message, error) {
+func (dc *DHCP6Clnterface) buildSolicit() (*dhcpv6.Message, error) {
 	var optModList []dhcpv6.Modifier
 	if dc.cfg.NeedNA {
 		optModList = append(optModList, dhcpv6.WithIAID(getIAIDviaTime(0)))
@@ -99,97 +113,70 @@ func (dc *DHCP6Clnt) buildSolicit() (*dhcpv6.Message, error) {
 	return m, nil
 }
 
-// Dial completes a DHCPv6 exchange with server
-func (dc *DHCP6Clnt) Dial() error {
-	checkResp := func(msg *dhcpv6.Message, record bool) error {
-		if dc.cfg.NeedNA {
-			if len(msg.Options.OneIANA().Options.Addresses()) == 0 {
-				return fmt.Errorf("no IANA address is assigned")
-			}
-			if record {
-				dc.assignedIANAs = []net.IP{}
-				for _, addr := range msg.Options.OneIANA().Options.Addresses() {
-					dc.assignedIANAs = append(dc.assignedIANAs, addr.IPv6Addr)
-				}
+func (dc *DHCP6Clnterface) checkResponse(msg *dhcpv6.Message, save bool) error {
+	if dc.cfg.NeedNA {
+		addresses := msg.Options.OneIANA().Options.Addresses()
+		if len(addresses) == 0 {
+			return errors.New("no IANA address is assigned")
+		}
+		if save {
+			for _, addr := range addresses {
+				dc.assignedIANAs = append(dc.assignedIANAs, addr.IPv6Addr)
 			}
 		}
+	}
 
-		if dc.cfg.NeedPD {
-			if len(msg.Options.OneIAPD().Options.Prefixes()) == 0 {
-				return fmt.Errorf("no IAPD prefix is assigned")
-			}
-			if record {
-				dc.assignedIAPDs = []*net.IPNet{}
-				for _, p := range msg.Options.OneIAPD().Options.Prefixes() {
-					dc.assignedIAPDs = append(dc.assignedIAPDs, p.Prefix)
-				}
+	if dc.cfg.NeedPD {
+		prefixes := msg.Options.OneIAPD().Options.Prefixes()
+		if len(prefixes) == 0 {
+			return errors.New("no IAPD prefix is assigned")
+		}
+		if save {
+			for _, p := range prefixes {
+				dc.assignedIAPDs = append(dc.assignedIAPDs, p.Prefix)
 			}
 		}
-		return nil
 	}
 
-	solicitMsg, err := dc.buildSolicit()
-	if err != nil {
-		return fmt.Errorf("failed to create solicit msg for %v, %v", dc.cfg.Mac, err)
-	}
-	adv, err := dc.clnt.SendAndRead(context.Background(),
-		nclient6.AllDHCPRelayAgentsAndServers, solicitMsg,
-		nclient6.IsMessageType(dhcpv6.MessageTypeAdvertise))
-	if err != nil {
-		return fmt.Errorf("failed recv DHCPv6 advertisement for %v, %v", dc.cfg.Mac, err)
-	}
-	if err := checkResp(adv, false); err != nil {
-		return fmt.Errorf("got invalid advertise msg for clnt %v, %v", dc.cfg.Mac, err)
-	}
-
-	request, err := newRequestFromAdv(adv)
-	if err != nil {
-		return fmt.Errorf("failed to build request msg for clnt %v, %v", dc.cfg.Mac, err)
-	}
-	reply, err := dc.clnt.SendAndRead(context.Background(),
-		nclient6.AllDHCPRelayAgentsAndServers,
-		request, nclient6.IsMessageType(dhcpv6.MessageTypeReply))
-	if err != nil {
-		return fmt.Errorf("failed to recv DHCPv6 reply for %v, %v", dc.cfg.Mac, err)
-	}
-
-	if err := checkResp(reply, true); err != nil {
-		return fmt.Errorf("got invalid reply msg for %v, %v", dc.cfg.Mac, err)
-	}
 	return nil
 }
 
 func newRequestFromAdv(adv *dhcpv6.Message, modifiers ...dhcpv6.Modifier) (*dhcpv6.Message, error) {
 	if adv == nil {
-		return nil, fmt.Errorf("ADVERTISE cannot be nil")
+		return nil, errors.New("ADVERTISE cannot be nil")
+	} else if adv.MessageType != dhcpv6.MessageTypeAdvertise {
+		return nil, errors.New("the passed ADVERTISE must have ADVERTISE type set")
 	}
-	if adv.MessageType != dhcpv6.MessageTypeAdvertise {
-		return nil, fmt.Errorf("the passed ADVERTISE must have ADVERTISE type set")
-	}
+
 	// build REQUEST from ADVERTISE
 	req, err := dhcpv6.NewMessage()
 	if err != nil {
 		return nil, err
 	}
 	req.MessageType = dhcpv6.MessageTypeRequest
+
 	// add Client ID
 	cid := adv.GetOneOption(dhcpv6.OptionClientID)
 	if cid == nil {
-		return nil, fmt.Errorf("client ID cannot be nil in ADVERTISE when building REQUEST")
+		return nil, errors.New("client ID cannot be nil in ADVERTISE when building REQUEST")
 	}
 	req.AddOption(cid)
+
 	// add Server ID
 	sid := adv.GetOneOption(dhcpv6.OptionServerID)
 	if sid == nil {
-		return nil, fmt.Errorf("server ID cannot be nil in ADVERTISE when building REQUEST")
+		return nil, errors.New("server ID cannot be nil in ADVERTISE when building REQUEST")
 	}
 	req.AddOption(sid)
+
 	// add Elapsed Time
 	req.AddOption(dhcpv6.OptElapsedTime(0))
+
 	// add IA_NA
-	if iana := adv.Options.OneIANA(); iana != nil {
-		req.AddOption(iana)
+	if iaNa := adv.Options.OneIANA(); iaNa != nil {
+		req.AddOption(iaNa)
 	}
+
 	// add IA_PD
 	if iaPd := adv.GetOneOption(dhcpv6.OptionIAPD); iaPd != nil {
 		req.AddOption(iaPd)
@@ -198,9 +185,9 @@ func newRequestFromAdv(adv *dhcpv6.Message, modifiers ...dhcpv6.Modifier) (*dhcp
 		dhcpv6.OptionDNSRecursiveNameServer,
 		dhcpv6.OptionDomainSearchList,
 	))
+
 	// add OPTION_VENDOR_CLASS, only if present in the original request
-	vClass := adv.GetOneOption(dhcpv6.OptionVendorClass)
-	if vClass != nil {
+	if vClass := adv.GetOneOption(dhcpv6.OptionVendorClass); vClass != nil {
 		req.AddOption(vClass)
 	}
 
@@ -209,4 +196,11 @@ func newRequestFromAdv(adv *dhcpv6.Message, modifiers ...dhcpv6.Modifier) (*dhcp
 		mod(req)
 	}
 	return req, nil
+}
+
+func getIAIDviaTime(delta int64) (r [4]byte) {
+	buf := make([]byte, binary.MaxVarintLen64)
+	binary.PutVarint(buf, time.Now().UnixNano()+delta)
+	copy(r[:], buf[:4])
+	return r
 }
